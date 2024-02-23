f12d456f9ee143c424114783c4dca6757a3469b5
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2024-02-23 16:42:08 +0100

    Changed Sitemap Location + Next Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/(.*)/',
+          destination: '/',
+          permanent: true,
+        },        
+        {
+            source: '/home',
+            destination: '/',
+            permanent: true,
+        },
+        {
+            source: '/index',
+            destination: '/',
+            permanent: true,
+        },
+        {
+            source: '/project',
+            destination: '/projecten',
+            permanent: true,
+        }
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
