7d7b99c655b1d297e11919842ddf3a13ef0396ce
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2024-03-25 09:41:15 +0100

    Fixed Navbar Icon Bug

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -32,7 +32,7 @@ const Navbar = ({ toggle }: { toggle: () => void }) => (
           >
             <path
               fill="#fff"
-              d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z" // Hamburger icon
+              d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
             />
           </svg>
         </button>
